649fff989624bf69a1cd2cfa71f9c28408f7d3ca
Brandon Lim <brandonlim10p@icloud.com>
2023-07-18 17:17:22 -0700

    components

---
+++ brandonlimbjj/src/components/page404.js
@@ -0,0 +1,24 @@
+import page404img from "../page404.jpg";
+import "../App.css";
+
+const PageNotFound = () => {
+  return (
+    <>
+      <img className="bg404" src={page404img} alt="" />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-2 my-0 text-center">
+            <p className="lead m-0 fw-bolder" style={{ fontSize: "10em" }}>
+              404
+            </p>
+            <p className="lead m-0 fw-bold">
+              URL "{window.location.href}" does not exist.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PageNotFound;
